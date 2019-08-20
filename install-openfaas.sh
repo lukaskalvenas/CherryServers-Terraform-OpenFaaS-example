ea6aa744cfeb1d765d0d8fcb3c0cc769e5c252e7
lukaskalvenas <54058403+lukaskalvenas@users.noreply.github.com>
2019-08-20 16:34:58 +0300

    Update install-openfaas.sh

--- install-openfaas.sh
+++ install-openfaas.sh
@@ -3,6 +3,5 @@ cd ~
 sudo apt install -y curl
 sudo apt install -y git
 sudo git clone https://github.com/openfaas/faas.git
-sudo curl -sSL -o faas-cli.sh https://cli.openfaas.com
 sudo chmod +x faas-cli.sh
 sudo ./faas-cli.sh
